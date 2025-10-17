27285ee9b6c337a672358cc5d6e6fff8d9a564d7
francescoshox <157975846+francescoshox@users.noreply.github.com>
2025-10-17 13:39:56 +0200

    Fixed smoke test.

---
+++ tests/test_smoke.py
@@ -0,0 +1,9 @@
+"""Smoke test module."""
+
+
+def test_smoke():
+    """A simple smoke test to ensure the testing framework is set up correctly."""
+    assert True
+
+
+# TODO: this test has to be removed once we have proper tests
